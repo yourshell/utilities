c87ac622ae25a2a6e4f0474d98a7866f0e037483
Egor <bronnikov.40@mail.ru>
2022-05-09 00:56:27 +0300

    Update comments and refactor code

--- calc/src/expression.h
+++ calc/src/expression.h
@@ -1,6 +1,11 @@
-#ifndef BUFSIZE
-#define BUFSIZE 256
-#endif
+/**
+ *  @Name
+ *  Math expression evaluator header
+ *  
+ *  @Description
+ *  This header contains all necessary functions for parse and evaluate an expression.
+ * */
+
 
 
 #ifndef _STDIO_H
@@ -23,15 +28,31 @@
 #endif
 
 
+// Structures
 #include "structures/number-stack.h"
 #include "structures/char-stack.h"
 
 
-/* *********************** */
-/* Working with expression */
-/* *********************** */
+// Colors and styles
+#define NC  "\x1B[0m"
+#define RED "\x1B[31m"
+#define HEADER "\033[95m"
 
-/* Distributes the order of operations */
+
+
+/**
+ *  @Synopsis
+ *  int precedence(char operator);
+ *
+ *  @Description
+ *  Set the precedence of operations.
+ *
+ *  @param operator: Operator symbol
+ *  @type operator: char
+ *
+ *  @return: The precedence of given operator
+ *  @rtype: int
+ */
 int precedence(char operator) {
 	if(operator == '+' || operator == '-')
 		return 1;
@@ -43,19 +64,33 @@ int precedence(char operator) {
 }
 
 
-/* Apply operator to numbers */
+
+/**
+ *  @Synopsis
+ *  void apply_operator(NumberStack **stack, char operator);
+ *
+ *  @Description
+ *  Applying operator for numbers on stack.
+ *
+ *  @param stack: Stack where do we get the values from
+ *  @type stack: NumberStack**
+ *  @param operator: Operator symbol
+ *  @type operator: char
+ *
+ *  @return: `side effect`: Pop elements from stack of numbers, apply operator to them and push to the stack
+ */
 void apply_operator(NumberStack **stack, char operator) {
-	if(operator < 0 ) {
+	if(operator < 0 ) {                 // If its unary operator
 		double x = number_pop(*stack);
 		switch(-operator) {
 			case '+': number_push(*stack, x); break;
 			case '-': number_push(*stack, -x); break;
 		}
-	} else {
+	} else {                            // If its arithmetic operator
 		double y = number_pop(*stack);
 		double x = number_pop(*stack);
 		if(operator == '/' && y == 0) {
-			printf("Error: Division by zero");
+			printf("%sError: Division by zero%s\n", RED, NC);
 		}
 		switch(operator) {
 			case '+': number_push(*stack, x + y); break;
@@ -67,28 +102,77 @@ void apply_operator(NumberStack **stack, char operator) {
 }
 
 
-/* Checks if an operator is unary */
+
+/**
+ *  @Synopsis
+ *  int is_unary(char c);
+ *
+ *  @Description
+ *  Checks if char is unary operator.
+ *
+ *  @param c: Char to be checked
+ *  @type c: char
+ *
+ *  @return: True (1) if char is unary operator and False (0) otherwise
+ *  @rtype: int
+ */
 int is_unary(char c) {
 	return c == '+' || c == '-';
 }
 
 
-/* Checks if a character is an operator */
+
+/**
+ *  @Synopsis
+ *  int is_operator(char c);
+ *
+ *  @Description
+ *  Checks if a char is an arithmetic operator.
+ *
+ *  @param c: Char to be checked
+ *  @type c: char
+ *
+ *  @return: True (1) if char is arithmetic operator and False (0) otherwise
+ *  @rtype: int
+ */
 int is_operator(char c) {
 	return c == '+' || c == '-' || c == '*' || c == '/'; 
 }
 
 
-/* Checks if a character is delimiter */
+
+/**
+ *  @Synopsis
+ *  int is_delimiter(char c);
+ *
+ *  @Description
+ *  Checks if a char is delimiter.
+ *
+ *  @param c: Char to be checked
+ *  @type c: char
+ *
+ *  @return: True (1) if char is delimiter and False (0) otherwise
+ *  @rtype: int
+ */
 int is_delimiter(char c) {
 	return c == ' ' || c == '\t' || c == '\v';
 }
 
 
 
-/* Checks for brackets match */
+/**
+ *  @Synopsis
+ *  void check_brackets(char *expression)
+ *
+ *  @Description
+ *  Exception for brackets match. If open brackets not equal to close brackets prints error message.
+ *
+ *  @param expression: Expression string
+ *  @type expression: char*
+ *
+ *  @return: `side effect`: Exception message
+ */
 void check_brackets(char *expression) {
-
 	int open_brackets = 0, close_brackets = 0;
 	int len = strlen(expression);
 
@@ -100,29 +184,41 @@ void check_brackets(char *expression) {
 	}
 
 	if(open_brackets != close_brackets) {
-		printf("error: number of opening brackets not equal to number of closing brackets");
+		printf("%sError: Number of opening brackets not equal to number of closing brackets%s\n", RED, NC);
 	}
 }
 
 
-/* Evaluates an expression */
+
+/**
+ *  @Synopsis
+ *  double evaluate(char *expression);
+ *
+ *  @Description
+ *  Evaluating the expression.
+ *
+ *  @param expression: Given expression
+ *  @type expression: char*
+ *
+ *  @return: The result of expression
+ *  @rtype: double
+ */
 double evaluate(char *expression) {
-	
-	check_brackets(expression);
-    NumberStack *values = create_number_stack(BUFSIZE);
-	CharStack *ops = create_char_stack(BUFSIZE);
+	check_brackets(expression);                 // Checks brackets match
+    NumberStack *values = create_number_stack(BUFSIZ);
+	CharStack *ops = create_char_stack(BUFSIZ);
 	int i, len = strlen(expression);
 	int may_be_unary = 0, break_point = 0;
 
 
-	for (i = 0; i < len; i++) {
-		if (is_delimiter(expression[i])) {	/* Current char from expression is delimiter */
+	for(i = 0; i < len; i++) {
+		if(is_delimiter(expression[i])) {       // Current char from expression is delimiter
 			continue;
-		} else if(expression[i] == '(') {	/* Current char from expression is open bracket */
+		} else if(expression[i] == '(') {	    // Current char from expression is open bracket
 			char_push(ops, expression[i]);
 			may_be_unary = 1;
-		} else if (isdigit(expression[i])) {	/* Current char from expression is roman number */
-			char val[BUFSIZE] = "";
+		} else if(isdigit(expression[i])) {     // Current char from expression is number
+			char val[BUFSIZ] = "";
 			while(i < len && (isdigit(expression[i]) || expression[i] == '.')) {
 				strncat(val, &expression[i], 1);
 				i++;
@@ -130,7 +226,7 @@ double evaluate(char *expression) {
 			number_push(values, atof(val));
 			i--;
 			may_be_unary = 0;
-		} else if (expression[i] == ')') {	/* Current char from expression is close bracket*/
+		} else if(expression[i] == ')') {       // Current char from expression is close bracket
 			while(!char_is_empty(ops) && char_peek(ops) != '(') {
 				char op = char_pop(ops);
 				apply_operator(&values, op);
@@ -138,7 +234,7 @@ double evaluate(char *expression) {
 			if(!char_is_empty(ops))
 				char_pop(ops);
 			may_be_unary = 0;
-		} else if (is_operator(expression[i])) {	/* Current char from expression is operator */
+		} else if(is_operator(expression[i])) { // Current char from expression is operator
 			char current_operator = expression[i];
 			if(may_be_unary && is_unary(current_operator))
 				current_operator = -current_operator;
@@ -151,8 +247,8 @@ double evaluate(char *expression) {
 			}
 			char_push(ops, current_operator);
 			may_be_unary = 1;
-		} else {	/* Current char from expression is unacceptable value for expression */
-			printf("Error: Bad input value %c\n", expression[i]);
+		} else {                                // Current char from expression is unacceptable value for expression
+			printf("%sError: Bad input value %c%s\n", RED, expression[i], NC);
             continue;
 		}
 	}
--- calc/src/main.c
+++ calc/src/main.c
@@ -1,37 +1,60 @@
+/**
+ * @Name
+ * Simple terminal calculator
+ *
+ * @Desciption
+ * Example of how to work with expression evaluator.
+ * */
+
+
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include "expression.h"
 
 
+
 char *readline(void);
 void input(void);
 
 
+//! main
 int main(void) {
     input();
 	return 0;
 }
 
 
+
+/**
+ *  @Synopsis
+ *  char* readline(void);
+ *
+ *  @Desciption
+ *  Read input line from stdin.
+ *
+ *  @return: Given string from stdin
+ *  @rtype: char*
+ */
 char *readline(void) {
-    int bufsize = BUFSIZE;
+    int bufsize = BUFSIZ;
     int position = 0;
     char *buffer = malloc(sizeof(char)*bufsize);
     int c;
 
-    if (!buffer) {
-        fprintf(stderr, "Calc: Allocation error\n");
+    if(!buffer) {
+        fprintf(stderr, "calc.c: Allocation error\n");
         exit(EXIT_FAILURE);
     }
 
-    while (1) {
+    while(1) {
         c = getchar();
 
-        if (c == '\n') {
+        if(c == '\n') {
             buffer[position] = '\0';
             return buffer;
-        } else if (c == EOF) {
+        } else if(c == EOF) {
             printf("\n");
             exit(EXIT_SUCCESS);
         } else {
@@ -39,11 +62,11 @@ char *readline(void) {
         }
         position++;
 
-        if (position >= bufsize) {
-            bufsize += BUFSIZE;
+        if(position >= bufsize) {
+            bufsize += BUFSIZ;
             buffer = realloc(buffer, bufsize);
-            if (!buffer) {
-                fprintf(stderr, "Calc: Allocation error\n");
+            if(!buffer) {
+                fprintf(stderr, "calc: Allocation error\n");
                 exit(EXIT_FAILURE);
             }
         }
@@ -51,17 +74,25 @@ char *readline(void) {
 }
 
 
+
+/**
+ *  @Synopsis
+ *  void input(void)
+ *
+ *  @Desciption
+ *  Example of express evaluator usage.
+ */
 void input(void) {
 	char *expression;
     
-    printf("calc.c\n");
+    printf("%scalc.c\n", HEADER);
     printf("Type some expressions and get results.\n");
-	printf("To finish entering expressions just type - 'end'\n\n\n");
+	printf("To finish entering expressions just type - 'end'%s\n\n\n", NC);
 
-	while (1) {
+	while(1) {
 		printf(">>> ");
         expression = readline();
-		if (strcmp(expression, "end")) {
+		if(strcmp(expression, "end")) {
             printf("%f\n", evaluate(expression));
 		} else {
 			break;
